289eeb3187023cea6875568a937b0d14cc70227f
Saurav Yadav <71329650+WizzCodes@users.noreply.github.com>
2021-05-13 20:59:50 +0530

    Add files via upload

---
+++ Tut4.py
@@ -0,0 +1,20 @@
+grocery = ("Sugar", "Bread", "Chicken Lollipop", "Jim jam", 65)
+#print(grocery)
+#print(grocery[3])number
+"""
+#numbers.sort()
+#print(numbers[::-2])
+#print(min(numbers))
+"""
+#numbers = [1, 2, 7, 6, 3]
+#numbers.append(6)
+#numbers.insert(1, 63)
+#numbers.remove(2)
+#numbers.pop()
+#print(numbers)
+#tp = (1,)
+#print(tp)
+a = 2
+b = 65
+a, b = b, a
+print(a, b)
